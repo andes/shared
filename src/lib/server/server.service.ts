3c469eec2c3ace2243be72318787648a158c0a80
Mariano Andres Botta <marianoabotta@gmail.com>
2019-07-10 09:28:11 -0300

    feat(server): Actualiza a HttpClientModule

--- src/lib/server/server.service.ts
+++ src/lib/server/server.service.ts
@@ -1,9 +1,13 @@
-import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+// import { RequestOptions, URLSearchParams } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 
 import { Plex } from '@andes/plex';
 import { Options } from './options';
+import { finalize, map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 // Constantes
 const defaultOptions: Options = { params: null, showError: true, showLoader: true };
@@ -12,7 +16,7 @@ const defaultOptions: Options = { params: null, showError: true, showLoader: tru
 export class Server {
     private baseURL: string;
 
-    constructor(private http: Http, private plex: Plex) { }
+    constructor(private http: HttpClient, private plex: Plex) { }
 
     private parse(data: any): any {
         let rvalidchars = /^[\],:{}\s]*$/;
@@ -46,26 +50,26 @@ export class Server {
         return JSON.stringify(object);
     }
 
-    private prepareOptions(options: Options): RequestOptions {
-        let result = new RequestOptions({
-            headers: new Headers({
+    private prepareOptions(options: Options) {
+        const result: any = {
+            headers: new HttpHeaders({
                 'Content-Type': 'application/json',
-                'Authorization': window.sessionStorage.getItem('jwt') ? 'JWT ' + window.sessionStorage.getItem('jwt') : null
+                'Authorization': window.sessionStorage.getItem('jwt') ? 'JWT ' + window.sessionStorage.getItem('jwt') : ''
             }),
-        });
+        };
         if (options && options.params) {
-            result.search = new URLSearchParams();
+            result.params = new HttpParams();
             for (let param in options.params) {
                 if (options.params[param] !== undefined) {
                     if (Array.isArray(options.params[param])) {
                         (options.params[param] as Array<any>).forEach((value) => {
-                            result.search.append(param, value);
+                            result.params = result.params.append(param, value);
                         });
                     } else {
                         if (options.params[param] instanceof Date) {
-                            result.search.set(param, (options.params[param] as Date).toISOString());
+                            result.params = result.params.set(param, (options.params[param] as Date).toISOString());
                         } else {
-                            result.search.set(param, options.params[param]);
+                            result.params = result.params.set(param, options.params[param]);
                         }
                     }
                 }
@@ -85,7 +89,7 @@ export class Server {
     }
 
     private handleError(error: any, options: Options) {
-        let message = (error && error.json().message) || 'La aplicación no pudo comunicarse con el servidor. Por favor revise su conexión a la red.';
+        let message = (error && JSON.parse(error.error).message) || 'La aplicación no pudo comunicarse con el servidor. Por favor revise su conexión a la red.';
         if (!options || options.showError || (options.showError === undefined)) {
             // El código 400 es usado para enviar mensaje de validación al usuario
             if (error.status === 400) {
@@ -94,7 +98,7 @@ export class Server {
                 this.plex.info('danger', `${message}<div class="text-muted small pt-3">Código de error: ${error.status}</div>`, 'No se pudo conectar con el servidor');
             }
         }
-        return Observable.throw(message);
+        return throwError(message);
     }
 
     private getAbsoluteURL(url: string) {
@@ -111,41 +115,46 @@ export class Server {
 
     get(url: string, options: Options = defaultOptions): Observable<any> {
         this.updateLoader(true, options);
-        return this.http.get(this.getAbsoluteURL(url), this.prepareOptions(options))
-            .finally(() => this.updateLoader(false, options))
-            .map((res: Response) => this.parse(res.text()))
-            .catch((err: any, caught: Observable<any>) => this.handleError(err, options));
+        return this.http.get(this.getAbsoluteURL(url), this.prepareOptions(options)).pipe(
+            finalize(() => this.updateLoader(false, options)),
+            map((res: any) => this.parse(res)),
+            catchError((err: any) => this.handleError(err, options))
+        );
     }
 
     post(url: string, body: any, options: Options = null): Observable<any> {
         this.updateLoader(true, options);
-        return this.http.post(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options))
-            .finally(() => this.updateLoader(false, options))
-            .map((res: Response) => this.parse(res.text()))
-            .catch((err: any, caught: Observable<any>) => this.handleError(err, options));
+        return this.http.post(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options)).pipe(
+            finalize(() => this.updateLoader(false, options)),
+            map((res: any) => this.parse(res)),
+            catchError((err: any) => this.handleError(err, options))
+        );
     }
 
     put(url: string, body: any, options: Options = defaultOptions): Observable<any> {
         this.updateLoader(true, options);
-        return this.http.put(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options))
-            .finally(() => this.updateLoader(false, options))
-            .map((res: Response) => this.parse(res.text()))
-            .catch((err: any, caught: Observable<any>) => this.handleError(err, options));
+        return this.http.put(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options)).pipe(
+            finalize(() => this.updateLoader(false, options)),
+            map((res: any) => this.parse(res)),
+            catchError((err: any) => this.handleError(err, options))
+        );
     }
 
     patch(url: string, body: any, options: Options = defaultOptions): Observable<any> {
         this.updateLoader(true, options);
-        return this.http.patch(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options))
-            .finally(() => this.updateLoader(false, options))
-            .map((res: Response) => this.parse(res.text()))
-            .catch((err: any, caught: Observable<any>) => this.handleError(err, options));
+        return this.http.patch(this.getAbsoluteURL(url), this.stringify(body), this.prepareOptions(options)).pipe(
+            finalize(() => this.updateLoader(false, options)),
+            map((res: any) => this.parse(res)),
+            catchError((err: any) => this.handleError(err, options))
+        );
     }
 
     delete(url: string, options: Options = defaultOptions): Observable<any> {
         this.updateLoader(true, options);
-        return this.http.delete(this.getAbsoluteURL(url), this.prepareOptions(options))
-            .finally(() => this.updateLoader(false, options))
-            .map((res: Response) => this.parse(res.text()))
-            .catch((err: any, caught: Observable<any>) => this.handleError(err, options));
+        return this.http.delete(this.getAbsoluteURL(url), this.prepareOptions(options)).pipe(
+            finalize(() => this.updateLoader(false, options)),
+            map((res: any) => this.parse(res)),
+            catchError((err: any) => this.handleError(err, options))
+        );
     }
 }
