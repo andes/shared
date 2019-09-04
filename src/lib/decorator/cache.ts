0d71679c1eaedef09a348d4aa8e338887e55370a
Mariano Andres Botta <marianoabotta@gmail.com>
2019-09-04 10:23:33 -0300

    feat: update cache decoratos

--- src/lib/decorator/cache.ts
+++ src/lib/decorator/cache.ts
@@ -2,18 +2,19 @@
  * Cachea el resultado de una request de forma automatica.
  * Se puede determinar la KEY de cache.
  */
-import { Observable } from 'rxjs/Rx';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-export function Cache ({key}) {
+export function Cache({ key }) {
     let _cache: any = {};
-    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const fn = descriptor.value as Function;
         descriptor.value = function (...args) {
             const objectKey = key ? (typeof key === 'string' ? args[0][key] : args[0]) : 'default';
             if (!_cache[objectKey]) {
-                return fn.apply(this, args).do(x => _cache[objectKey] = x);
+                return fn.apply(this, args).pipe(tap(x => _cache[objectKey] = x));
             } else {
-                return new Observable(resultado => resultado.next(_cache[objectKey]));
+                return of(_cache[objectKey]);
             }
         };
         return descriptor;
