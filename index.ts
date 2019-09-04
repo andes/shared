b7a995bd02595b4c43d34a3d71dd383a296fb2d2
Mariano Andres Botta <marianoabotta@gmail.com>
2019-09-04 10:24:25 -0300

    feat: export options

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from './src/lib/server/server.service';
 export * from './src/lib/decorator/cache';
 export * from './src/lib/decorator/unsubscribe';
 export * from './src/lib/shared.module';
+export * from './src/lib/decorator/observe';
+export * from './src/lib/operators';
