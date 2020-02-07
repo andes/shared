1d7d555b276e05f3c21773f8825f0f84258121b4
Mariano Andres Botta <marianoabotta@gmail.com>
2020-02-07 08:53:43 -0300

    fix(resource-base): add interfaces

--- src/lib/server/resource-base.ts
+++ src/lib/server/resource-base.ts
@@ -1,9 +1,33 @@
 import { Server } from './server.service';
 import { Observable } from 'rxjs';
 
+export interface ShowErrorDetail {
+    create?: boolean;
+    update?: boolean;
+    delete?: boolean;
+    get?: boolean;
+    search?: boolean;
+}
 
+/**
+ * Implementa las rutas básicas de ResourceBase
+ */
 export abstract class ResourceBaseHttp<T = any> {
+    /**
+     * Endpoint del recurso básico
+     */
     protected abstract url: string;
+
+    /**
+     * Muestra un cartel en caso de error
+     */
+
+    protected showError = true;
+
+    /**
+     * Permite customizar por ruta el cartel de error;
+     */
+    protected showErrorDetail: ShowErrorDetail = {}
 
     constructor(protected server: Server) { }
 
@@ -15,24 +39,31 @@ export abstract class ResourceBaseHttp<T = any> {
         }
     }
 
-    get(id): Observable<T> {
-        return this.server.get(`${this.url}/${id}`, { showError: false });
+    get(id: string): Observable<T> {
+        return this.server.get(`${this.url}/${id}`, { showError: this.mustShowError('get') });
     }
 
-    find(query = {}): Observable<T[]> {
-        return this.server.get(this.url, { params: query });
+    search(query = {}): Observable<T[]> {
+        return this.server.get(this.url, { params: query, showError: this.mustShowError('search') });
     }
 
     create(body): Observable<T> {
-        return this.server.post(this.url, body);
+        return this.server.post(this.url, body, { showError: this.mustShowError('create') });
     }
 
-    update(id, body): Observable<T> {
-        return this.server.patch(`${this.url}/${id}`, body);
+    update(id: string, body): Observable<T> {
+        return this.server.patch(`${this.url}/${id}`, body, { showError: this.mustShowError('update') });
     }
 
-    delete(id): Observable<T> {
-        return this.server.delete(`${this.url}/${id}`);
+    delete(id: string): Observable<T> {
+        return this.server.delete(`${this.url}/${id}`, { showError: this.mustShowError('delete') });
+    }
+
+    private mustShowError(type: string) {
+        if (this.showErrorDetail[type] !== undefined) {
+            return this.showErrorDetail[type];
+        }
+        return this.showError;
     }
 
 }
