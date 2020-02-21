6ccaf37f74b7f526a604d8eb39ff59301453f0c1
Mariano Andres Botta <marianoabotta@gmail.com>
2020-02-21 10:24:29 -0300

    feat(operators): agrega interfaces

--- src/lib/operators/index.ts
+++ src/lib/operators/index.ts
@@ -1,33 +1,35 @@
 import { distinctUntilChanged, map, scan, filter, publishReplay, refCount } from 'rxjs/operators';
-import { pipe } from 'rxjs';
+import { pipe, OperatorFunction } from 'rxjs';
 
-export function notNull() {
-    return filter(user => !!user);
+export function notNull<T>() {
+    return filter<T>(user => !!user);
 }
 
-export function onlyNull() {
-    return filter(user => !user);
+export function onlyNull<T>() {
+    return filter<T>(user => !user);
 }
 
 export function mergeObject() {
     return scan((acc, curr) => Object.assign({}, acc, curr), {});
 }
 
-export function asObject(key, fn) {
-    return map(value => {
+export function asObject<T, R = any>(key, fn = null) {
+    return map<T, R>(value => {
+        fn = fn || ((t) => t);
         const obj = {};
         obj[key] = value ? fn(value) : null;
-        return obj;
+        return obj as R;
     });
 }
+
 const replacer = (key, value) => value === null ? undefined : value;
-export function distincObject() {
-    return distinctUntilChanged((a, b) => {
+export function distincObject<T>() {
+    return distinctUntilChanged<T>((a, b) => {
         return JSON.stringify(a, replacer) === JSON.stringify(b, replacer);
     });
 }
 
-export function cache() {
+export function cache<T>(): OperatorFunction<T, T> {
     return pipe(
         publishReplay(1),
         refCount()
