159042981ce6e574072fd52040726d6dea896e85
Mariano Andres Botta <marianoabotta@gmail.com>
2020-06-26 11:34:30 -0300

    fix(cache): ajusta cache decorator

--- src/lib/decorator/cache.ts
+++ src/lib/decorator/cache.ts
@@ -2,20 +2,21 @@
  * Cachea el resultado de una request de forma automatica.
  * Se puede determinar la KEY de cache.
  */
-import { tap } from 'rxjs/operators';
-import { of } from 'rxjs';
 
-export function Cache({ key }) {
+import { cache } from '../operators';
+
+export function Cache({ key } = { key: false }) {
     let _cache: any = {};
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const fn = descriptor.value as Function;
         descriptor.value = function (...args) {
             const objectKey = key ? (typeof key === 'string' ? args[0][key] : args[0]) : 'default';
             if (!_cache[objectKey]) {
-                return fn.apply(this, args).pipe(tap(x => _cache[objectKey] = x));
-            } else {
-                return of(_cache[objectKey]);
+                _cache[objectKey] = fn.apply(this, args).pipe(
+                    cache()
+                );
             }
+            return _cache[objectKey];
         };
         return descriptor;
     };
